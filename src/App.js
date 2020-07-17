d5801aa6fb153496047024eb33d4c9c184de4a12
JungHyeokChoi <page1114@gmail.com>
2020-07-17 17:54:31 +0900

    Simple Ether Send Web

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const web3 = new Web3(window.ethereum);
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      balance: "<Fetching balance>"
+    };
+
+    (async () => {
+      const balance = await web3.eth.getBalance("0xe14466f04e13764dc0E28cd954848c68b2260C46");
+      this.setState({ balance: `${web3.utils.fromWei(balance)} ETH`})
+    })();
+  }
+
+  sendEther(e) {
+    e.preventDefault();
+    window.ethereum.enable();
+
+    (async () => {
+      await web3.eth.sendTransaction({
+        from: window.ethereum.selectedAddress,
+        to: this.to.value,
+        value: web3.utils.toWei(this.value.value)
+      });
+    })();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{this.state.balance}</p>
+          <form onSubmit={(e) => this.sendEther(e)}>
+            <input placeholder="보낼 주소" ref={(input) => this.to = input} style={{width: "500px"}}/>
+            <br />
+            <input placeholder="보낼 금액" ref={(input) => this.value = input} style={{width: "500px"}}/>
+            <br />
+            <button type="submit">Send</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
